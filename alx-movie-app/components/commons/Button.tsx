b671060b1b9e556cef4123522af19d86d74d6359
Lord-Abiolla <abiollajames@protonmail.com>
2025-12-06 17:42:50 +0300

    Bootstraping the MovieApp

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button() {
+    return (
+        <h1>Button here</h1>
+    )
+}
+
+export default Button;
---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MovieCard() {
+    return (
+        <h1>MovieCard here</h1>
+    )
+}
+
+export default MovieCard;
